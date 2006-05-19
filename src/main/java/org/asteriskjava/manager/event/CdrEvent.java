0d7e1e119d5aee58e192126d762fdaeee245402f
Stefan Reuter <stefan.reuter@reucon.com>
2006-05-19 12:14:32 +0000

    Added JavaDoc

--- src/main/java/org/asteriskjava/manager/event/CdrEvent.java
+++ src/main/java/org/asteriskjava/manager/event/CdrEvent.java
@@ -58,181 +58,383 @@ public class CdrEvent extends ManagerEvent
         super(source);
     }
 
+    /**
+     * Returns the account number that is usually used to identify the party to bill for the call.<br>
+     * Corresponds to CDR field <code>accountcode</code>.
+     * 
+     * @return the account number.
+     */
     public String getAccountCode()
     {
         return accountCode;
     }
 
+    /**
+     * Sets the account number.
+     * 
+     * @param accountCode the account number.
+     */
     public void setAccountCode(String accountCode)
     {
         this.accountCode = accountCode;
     }
 
+    /**
+     * Returns the Caller*ID number.<br>
+     * Corresponds to CDR field <code>src</code>.
+     * 
+     * @return the Caller*ID number.
+     */
     public String getSrc()
     {
         return src;
     }
 
+    /**
+     * Sets the Caller*ID number.
+     * 
+     * @param source the Caller*ID number.
+     */
     public void setSrc(String source)
     {
         this.src = source;
     }
 
+    /**
+     * Returns the destination extension.<br>
+     * Corresponds to CDR field <code>dst</code>.
+     * 
+     * @return the destination extension.
+     */
     public String getDestination()
     {
         return destination;
     }
 
+    /**
+     * Sets the destination extension.
+     * 
+     * @param destination the destination extension.
+     */
     public void setDestination(String destination)
     {
         this.destination = destination;
     }
 
+    /**
+     * Returns the destination context.<br>
+     * Corresponds to CDR field <code>dcontext</code>.
+     * 
+     * @return the destination context.
+     */
     public String getDestinationContext()
     {
         return destinationContext;
     }
 
+    /**
+     * Sets the destination context.
+     * 
+     * @param destinationContext the destination context.
+     */
     public void setDestinationContext(String destinationContext)
     {
         this.destinationContext = destinationContext;
     }
 
+    /**
+     * Returns the Caller*ID with text.<br>
+     * Corresponds to CDR field <code>clid</code>.
+     * 
+     * @return the Caller*ID with text
+     */
     public String getCallerId()
     {
         return callerId;
     }
 
+    /**
+     * Sets the Caller*ID with text.
+     * 
+     * @param callerId the Caller*ID with text.
+     */
     public void setCallerId(String callerId)
     {
         this.callerId = callerId;
     }
 
+    /**
+     * Returns the name of the channel, for example "SIP/1310-asfe".<br>
+     * Corresponds to CDR field <code>channel</code>.
+     * 
+     * @return the name of the channel.
+     */
     public String getChannel()
     {
         return channel;
     }
 
+    /**
+     * Sets the name of the channel.
+     * 
+     * @param channel the name of the channel.
+     */
     public void setChannel(String channel)
     {
         this.channel = channel;
     }
 
+    /**
+     * Returns the name of the destination channel if appropriate.<br>
+     * Corresponds to CDR field <code>dstchannel</code>.
+     * 
+     * @return the name of the destination channel or <code>null</code> if not available.
+     */
     public String getDestinationChannel()
     {
         return destinationChannel;
     }
 
+    /**
+     * Sets the name of the destination channel.
+     * 
+     * @param destinationChannel the name of the destination channel.
+     */
     public void setDestinationChannel(String destinationChannel)
     {
         this.destinationChannel = destinationChannel;
     }
 
+    /**
+     * Returns the last application if appropriate, for example "VoiceMail".<br>
+     * Corresponds to CDR field <code>lastapp</code>.
+     * 
+     * @return the last application or <code>null</code> if not avaialble.
+     */
     public String getLastApplication()
     {
         return lastApplication;
     }
 
+    /**
+     * Sets the last application.
+     * 
+     * @param lastApplication the last application.
+     */
     public void setLastApplication(String lastApplication)
     {
         this.lastApplication = lastApplication;
     }
 
+    /**
+     * Returns the last application's data (arguments), for example "s1234".<br>
+     * Corresponds to CDR field <code>lastdata</code>.
+     * 
+     * @return the last application's data or <code>null</code> if not avaialble.
+     */
     public String getLastData()
     {
         return lastData;
     }
 
+    /**
+     * Set the last application's data.
+     * 
+     * @param lastData the last application's data.
+     */
     public void setLastData(String lastData)
     {
         this.lastData = lastData;
     }
 
+    /**
+     * Returns when the call has started.<br>
+     * This corresponds to CDR field <code>start</code>.
+     * 
+     * @return A string of the format "%Y-%m-%d %T" (strftime(3)) representing the date/time the
+     * call has started, for example "2006-05-19 11:54:48".
+     */
     public String getStartTime()
     {
         return startTime;
     }
 
+    /**
+     * Sets the date/time when the call has started.
+     * 
+     * @param startTime the date/time when the call has started.
+     */
     public void setStartTime(String startTime)
     {
         this.startTime = startTime;
     }
 
+    /**
+     * Returns when the call was answered.<br>
+     * This corresponds to CDR field <code>answered</code>.
+     * 
+     * @return A string of the format "%Y-%m-%d %T" (strftime(3)) representing the date/time the
+     * call was answered, for example "2006-05-19 11:55:01"
+     */
     public String getAnswerTime()
     {
         return answerTime;
     }
 
+    /**
+     * Sets the date/time when the call was answered.
+     * 
+     * @param answerTime the date/time when the call was answered.
+     */
     public void setAnswerTime(String answerTime)
     {
         this.answerTime = answerTime;
     }
 
+    /**
+     * Returns when the call has ended.<br>
+     * This corresponds to CDR field <code>end</code>.
+     * 
+     * @return A string of the format "%Y-%m-%d %T" (strftime(3)) representing the date/time the
+     * call has ended, for example "2006-05-19 11:58:21"
+     */
     public String getEndTime()
     {
         return endTime;
     }
 
+    /**
+     * Sets the date/time when the call has ended.
+     * 
+     * @param endTime the date/time when the call has ended.
+     */
     public void setEndTime(String endTime)
     {
         this.endTime = endTime;
     }
 
+    /**
+     * Returns the total time (in seconds) the caller spent in the system from dial to hangup.<br>
+     * Corresponds to CDR field <code>duration</code>.
+     * 
+     * @return the total time in system in seconds.
+     */
     public Integer getDuration()
     {
         return duration;
     }
 
+    /**
+     * Sets the total time in system.
+     * 
+     * @param duration total time in system in seconds.
+     */
     public void setDuration(Integer duration)
     {
         this.duration = duration;
     }
 
+    /**
+     * Returns the total time (in seconds) the call was up from answer to hangup.<br>
+     * Corresponds to CDR field <code>billsec</code>.
+     * 
+     * @return the total time in call in seconds.
+     */
     public Integer getBillableSeconds()
     {
         return billableSeconds;
     }
 
+    /**
+     * Sets the total time in call.
+     * 
+     * @param billableSeconds the total time in call in seconds.
+     */
     public void setBillableSeconds(Integer billableSeconds)
     {
         this.billableSeconds = billableSeconds;
     }
 
+    /**
+     * Returns what happened to the call.<br>
+     * This is one of "NO ANSWER", "FAILED", "BUSY", "ANSWERED" or "UNKNOWN".<br>
+     * Corresponds to CDR field <code>disposition</code>.
+     * 
+     * @return the disposition.
+     */
     public String getDisposition()
     {
         return disposition;
     }
 
+    /**
+     * Sets the disposition.
+     * 
+     * @param disposition the disposition.
+     */
     public void setDisposition(String disposition)
     {
         this.disposition = disposition;
     }
 
+    /**
+     * Returns the AMA (Automated Message Accounting) flags.<br>
+     * This is one of "OMIT", "BILLING", "DOCUMENTATION" or "Unknown".<br>
+     * Corresponds to CDR field <code>amaflags</code>.
+     * 
+     * @return the AMA flags.
+     */
     public String getAmaFlags()
     {
         return amaFlags;
     }
 
+    /**
+     * Sets the AMA (Automated Message Accounting) flags.
+     * 
+     * @param amaFlags the AMA (Automated Message Accounting) flags.
+     */
     public void setAmaFlags(String amaFlags)
     {
         this.amaFlags = amaFlags;
     }
 
+    /**
+     * Returns the unique id of the channel.
+     * 
+     * @return the unique id of the channel.
+     */
     public String getUniqueId()
     {
         return uniqueId;
     }
 
+    /**
+     * Sets the unique id of the channel.
+     * 
+     * @param uniqueId the unique id of the channel.
+     */
     public void setUniqueId(String uniqueId)
     {
         this.uniqueId = uniqueId;
     }
 
+    /**
+     * Returns the user-defined field as set by <code>Set(CDR(userfield)=Value)</code>.<br>
+     * Corresponds to CDR field <code>userfield</code>.
+     * 
+     * @return the user-defined field.
+     */
     public String getUserField()
     {
         return userField;
     }
 
+    /**
+     * Sets the user-defined field.
+     * 
+     * @param userField the user-defined field
+     */
     public void setUserField(String userField)
     {
         this.userField = userField;
