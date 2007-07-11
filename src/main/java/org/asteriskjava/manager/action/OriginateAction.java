79248410bd2a143ab619afef16f7993a438cb006
Stefan Reuter <stefan.reuter@reucon.com>
2007-07-11 09:54:20 +0000

    Fixed javadoc

--- src/main/java/org/asteriskjava/manager/action/OriginateAction.java
+++ src/main/java/org/asteriskjava/manager/action/OriginateAction.java
@@ -38,14 +38,12 @@ import org.asteriskjava.manager.event.OriginateResponseEvent;
  * If you set async to <code>true</code> Asterisk reports an OriginateSuccess-
  * and OriginateFailureEvents. The action id of these events equals the action
  * id of this OriginateAction.
- * 
- * @see org.asteriskjava.manager.event.OriginateResponseEvent
+ *
  * @author srt
  * @version $Id$
+ * @see org.asteriskjava.manager.event.OriginateResponseEvent
  */
-public class OriginateAction extends AbstractManagerAction
-        implements
-            EventGeneratingAction
+public class OriginateAction extends AbstractManagerAction implements EventGeneratingAction
 {
     /**
      * Serializable version identifier
@@ -67,15 +65,19 @@ public class OriginateAction extends AbstractManagerAction
 
     /**
      * Returns the name of this action, i.e. "Originate".
+     *
+     * @return the name of this action.
      */
     @Override
-   public String getAction()
+    public String getAction()
     {
         return "Originate";
     }
 
     /**
      * Returns the account code to use for the originated call.
+     *
+     * @return the account code to use for the originated call.
      */
     public String getAccount()
     {
@@ -86,6 +88,8 @@ public class OriginateAction extends AbstractManagerAction
      * Sets the account code to use for the originated call.<p>
      * The account code is included in the call detail record generated for this
      * call and will be used for billing.
+     *
+     * @param account the account code to use for the originated call.
      */
     public void setAccount(String account)
     {
@@ -104,7 +108,7 @@ public class OriginateAction extends AbstractManagerAction
      * Sets the caller id to set on the outgoing channel.<p>
      * This includes both the Caller*Id Number and Caller*Id Name in the form
      * "Jon Doe &lt;1234&gt;".
-     * 
+     *
      * @param callerId the caller id to set on the outgoing channel.
      */
     public void setCallerId(String callerId)
@@ -115,7 +119,7 @@ public class OriginateAction extends AbstractManagerAction
     /**
      * Returns the calling presentation for the outgoing channel.<p>
      * This property is only available on BRIstuffed Asterisk servers.
-     * 
+     *
      * @return the calling presentation for the outgoing channel.
      * @see #setCallingPres(Integer)
      */
@@ -127,16 +131,16 @@ public class OriginateAction extends AbstractManagerAction
     /**
      * Sets the calling presentation for the outgoing channel.<p>
      * The number is an octet and the only bits you need worry about are bits 1,2,6 and 7.<p>
-     * Bits 1 and 2 define the screening indicator and bits 6 and 7 define the 
+     * Bits 1 and 2 define the screening indicator and bits 6 and 7 define the
      * presentation indicator.<p>
-     * In essence, it says, 'Is the person who has been called allowed to see the callers number?' 
+     * In essence, it says, 'Is the person who has been called allowed to see the callers number?'
      * (presentation) and 'What authority was used to verify that this is a genuine number?'
      * (screening).<p>
-     * <p>
+     * <p/>
      * Presentation indicator (Bits 6 and 7):
      * <pre>
      * Bits Meaning
-     *  7 6 
+     *  7 6
      *  0 0 Presentation allowed
      *  0 1 Presentation restricted
      *  1 0 Number not available due to interworking
@@ -145,7 +149,7 @@ public class OriginateAction extends AbstractManagerAction
      * Screening indicator (Bits 1 and 2):
      * <pre>
      * Bits Meaning
-     *  2 1 
+     *  2 1
      *  0 0 User-provided, not screened
      *  0 1 User-provided, verified and passed
      *  1 0 User-provided, verified and failed
@@ -157,9 +161,9 @@ public class OriginateAction extends AbstractManagerAction
      * Presentation Restricted, User-provided, not screened: 32 (00100000)
      * Presentation Restricted, User-provided, verified, and passed: 33 (00100001)
      * Presentation Restricted, Network Provided: 35 (00100011)
-     * </pre> 
+     * </pre>
      * This property is only available on BRIstuffed Asterisk servers.
-     * 
+     *
      * @param callingPres the calling presentation for the outgoing channel.
      */
     public void setCallingPres(Integer callingPres)
@@ -282,7 +286,7 @@ public class OriginateAction extends AbstractManagerAction
      * The channel must be answered within this time, otherwise the origination
      * is considered to have failed and an OriginateFailureEvent is generated.<p>
      * If not set, Asterisk assumes a default value of 30000 meaning 30 seconds.
-     * 
+     *
      * @param timeout the timeout in milliseconds
      * @deprecated use {@see #setTimeout(Long)} instead.
      */
@@ -296,7 +300,7 @@ public class OriginateAction extends AbstractManagerAction
      * The channel must be answered within this time, otherwise the origination
      * is considered to have failed and an OriginateFailureEvent is generated.<p>
      * If not set, Asterisk assumes a default value of 30000 meaning 30 seconds.
-     * 
+     *
      * @param timeout the timeout in milliseconds
      */
     public void setTimeout(Long timeout)
@@ -310,7 +314,7 @@ public class OriginateAction extends AbstractManagerAction
      * multiple variable assignments separated by the '|' character.<p>
      * Example: "VAR1=abc|VAR2=def" sets the channel variables VAR1 to "abc" and
      * VAR2 to "def".
-     * 
+     *
      * @deprecated use {@link #setVariables(Map)} instead.
      */
     public void setVariable(String variable)
@@ -342,8 +346,8 @@ public class OriginateAction extends AbstractManagerAction
 
     /**
      * Sets an variable on the originated call.
-     * 
-     * @param name the name of the variable to set.
+     *
+     * @param name  the name of the variable to set.
      * @param value the value of the variable to set.
      * @since 0.3
      */
@@ -359,9 +363,9 @@ public class OriginateAction extends AbstractManagerAction
 
     /**
      * Returns the variables to set on the originated call.
-     * 
-     * @param variables a Map containing the variable names as key and their
-     *            values as value.
+     *
+     * @return a Map containing the variable names as key and their
+     *         values as value.
      * @since 0.2
      */
     public Map<String, String> getVariables()
@@ -371,9 +375,9 @@ public class OriginateAction extends AbstractManagerAction
 
     /**
      * Sets the variables to set on the originated call.
-     * 
+     *
      * @param variables a Map containing the variable names as key and their
-     *            values as value.
+     *                  values as value.
      * @since 0.2
      */
     public void setVariables(Map<String, String> variables)
