d6a01284a8a0d859ffe2197c4b68e517ed94f5c9
Alexander Polakov <apolyakov@beget.ru>
2015-01-23 20:43:12 +0300

    DestCallerIdNum on dialbegin

--- src/main/java/org/asteriskjava/manager/event/DialBeginEvent.java
+++ src/main/java/org/asteriskjava/manager/event/DialBeginEvent.java
@@ -1,5 +1,15 @@
 package org.asteriskjava.manager.event;
 
 public class DialBeginEvent extends DialEvent {
+    private String destCallerIdNum;
+
     public DialBeginEvent(Object source) { super(source); setSubEvent("Begin"); }
+
+    public String getDestCallerIdNum() {
+        return destCallerIdNum;
+    }
+
+    public void setDestCallerIdNum(String destCallerIdNum) {
+        this.destCallerIdNum = destCallerIdNum;
+    }
 }
