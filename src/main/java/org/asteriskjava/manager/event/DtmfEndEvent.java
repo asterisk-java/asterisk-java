3c5aa228b2f4310a84eef416e1155db23bdf1846
Alexander Polakov <apolyakov@beget.ru>
2015-01-26 14:23:16 +0300

    DurationMs property

--- src/main/java/org/asteriskjava/manager/event/DtmfEndEvent.java
+++ src/main/java/org/asteriskjava/manager/event/DtmfEndEvent.java
@@ -4,5 +4,14 @@ package org.asteriskjava.manager.event;
  * Created by Alexander Polakov <apolyakov@beget.ru> on 1/26/15.
  */
 public class DtmfEndEvent extends DtmfEvent {
+    private Integer durationMs;
     public DtmfEndEvent(Object source) { super(source); setBegin(false); setEnd(true); }
+
+    public Integer getDurationMs() {
+        return durationMs;
+    }
+
+    public void setDurationMs(Integer durationMs) {
+        this.durationMs = durationMs;
+    }
 }
