ada9f28ae46c2b42d49c66c444afab4e830f097a
Alexander Polakov <apolyakov@beget.ru>
2015-01-26 15:18:13 +0300

    BridgeEnter: uniqueId & channel

--- src/main/java/org/asteriskjava/manager/event/BridgeEnterEvent.java
+++ src/main/java/org/asteriskjava/manager/event/BridgeEnterEvent.java
@@ -4,5 +4,15 @@ package org.asteriskjava.manager.event;
  * Created by Alexander Polakov <apolyakov@beget.ru> on 1/26/15.
  */
 public class BridgeEnterEvent extends AbstractBridgeEvent {
+    private String channel;
+
     public BridgeEnterEvent(Object source) { super(source); }
+
+    public String getChannel() {
+        return channel;
+    }
+
+    public void setChannel(String channel) {
+        this.channel = channel;
+    }
 }
