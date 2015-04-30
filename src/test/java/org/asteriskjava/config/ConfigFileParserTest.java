c593448aa250aecfce5a92724ba1eedffcccbf50
bsutton@noojee.com.au <bsutton@noojee.com.au>
2015-05-01 09:20:40 +1000

    removed references to deprecated class.

--- src/test/java/org/asteriskjava/config/ConfigFileParserTest.java
+++ src/test/java/org/asteriskjava/config/ConfigFileParserTest.java
@@ -1,8 +1,8 @@
 package org.asteriskjava.config;
 
-import static junit.framework.Assert.assertEquals;
-import static junit.framework.Assert.assertTrue;
-import static junit.framework.Assert.fail;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 import java.nio.CharBuffer;
 
--- src/test/java/org/asteriskjava/fastagi/internal/AgiReplyImplTest.java
+++ src/test/java/org/asteriskjava/fastagi/internal/AgiReplyImplTest.java
@@ -16,8 +16,8 @@
  */
 package org.asteriskjava.fastagi.internal;
 
-import static junit.framework.Assert.assertEquals;
-import static junit.framework.Assert.assertFalse;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 
 import java.util.ArrayList;
 import java.util.List;
