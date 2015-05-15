67cb25903ad49fa4403ba6fbefa8836198596397
pet <pet@localhost>
2015-05-15 15:05:13 +0200

    formatting more in asteriskjava style

--- src/main/java/org/asteriskjava/manager/action/MessageSendAction.java
+++ src/main/java/org/asteriskjava/manager/action/MessageSendAction.java
@@ -4,8 +4,7 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 
 /**
- * Send a custom SIP message to the specified peer.
- * Available since Asterisk 11
+ * Send a custom SIP message to the specified peer. Available since Asterisk 11
  *
  * @author Michael Will
  * @version $Id$
@@ -14,87 +13,136 @@ import java.util.Map;
 public class MessageSendAction extends AbstractManagerAction
 {
 
-  private static final long serialVersionUID = -6554282385166411723L;
-  private String to;
-  private String from;
-  private String body;
-  private String base64body;  
-  private Map<String, String> variables;
-  
-  
-  @Override
-  public String getAction ()
-  {
-    return "MessageSend";
-  }
-  
-  /**
-   * Returns the variables to set on the originated call.
-   *
-   * @return a Map containing the variable names as key and their
-   *         values as value.
-   * @since 1.0.0
-   */
-  public Map<String, String> getVariables()
-  {
-      return variables;
-  }
+    private static final long serialVersionUID = -6554282385166411723L;
+    private String to;
+    private String from;
+    private String body;
+    private String base64body;
+    private Map<String, String> variables;
 
-  /**
-   * Sets an variable on the originated call.
-   *
-   * @param name  the name of the variable to set.
-   * @param value the value of the variable to set.
-   * @since 1.0.0
-   */
-  public void setVariable(String name, String value)
-  {
-      if (variables == null)
-      {
-          variables = new LinkedHashMap<String, String>();
-      }
+    @Override
+    public String getAction()
+    {
+        return "MessageSend";
+    }
 
-      variables.put(name, value);
-  }
-  
-  public String getTo ()
-  {
-    return to;
-  }
+    /**
+     * Returns the variables to set on the originated call.
+     *
+     * @return a Map containing the variable names as key and their values as
+     * value.
+     * @since 1.0.0
+     */
+    public Map<String, String> getVariables()
+    {
+        return variables;
+    }
 
-  public void setTo (String to)
-  {
-    this.to = to;
-  }
+    /**
+     * Sets an variable on the originated call.
+     *
+     * @param name the name of the variable to set.
+     * @param value the value of the variable to set.
+     * @since 1.0.0
+     */
+    public void setVariable(String name, String value)
+    {
+        if (variables == null)
+        {
+            variables = new LinkedHashMap<String, String>();
+        }
 
-  public String getFrom ()
-  {
-    return from;
-  }
+        variables.put(name, value);
+    }
 
-  public void setFrom (String from)
-  {
-    this.from = from;
-  }
+    /**
+     * Retrives destination of message
+     * 
+     * @return String in format like sip:userid
+     */
+    public String getTo()
+    {
+        return to;
+    }
 
-  public String getBody ()
-  {
-    return body;
-  }
+    /**
+     * Set destination of message
+     * 
+     * @param to String in format like sip:userid
+     */
+    public void setTo(String to)
+    {
+        this.to = to;
+    }
 
-  public void setBody (String body)
-  {
-    this.body = body;
-  }
+    /**
+     * get From String
+     * 
+     * @return String in form like  sip:asterisk@server-ip
+     */
+    public String getFrom()
+    {
+        return from;
+    }
 
-  public String getBase64body ()
-  {
-    return base64body;
-  }
+    /**
+     * set From String 
+     * @param String from in form like  sip:asterisk@server-ip
+     */
+    public void setFrom(String from)
+    {
+        this.from = from;
+    }
 
-  public void setBase64body (String base64body)
-  {
-    this.base64body = base64body;
-  }
+    /**
+     * Content of the message in plain text ascii
+     * Please consider to use getBase64Body instead to avoid encoding problems
+     * 
+     * @return String with Message content
+     */
+    public String getBody()
+    {
+        return body;
+    }
+
+    /**
+     * Sets the content of the message
+     * Please consider to use setBase64Body instead to avoid encoding problems
+     * 
+     * @param body String with plain ascii content
+     */
+    public void setBody(String body)
+    {
+        this.body = body;
+    }
+
+    /**
+     * Retrieves content encoded in base64
+     * 
+     * @code new String(Base64.decode(getBase64body(), Formatting (for ex "UTF-8"))
+     * @return Base64 encoded String
+     */
+    public String getBase64body()
+    {
+        return base64body;
+    }
+
+    /**
+     * Sets the content of the message body encode in base64
+     * 
+     * {@code    MessageSendAction sipSendMessage = new MessageSendAction();
+     *           sipSendMessage.setTo("sip:phoneuserid");
+     *           sipSendMessage.setVariable("Content-Type", "text/plain");
+     *           sipSendMessage.setVariable("P-hint", "usrloc applied");
+     * 
+     *           sipSendMessage.setActionId(UUID.randomUUID().toString());
+     *           sipSendMessage.setBase64body(new String(Base64.encodeBase64(message.getBytes("UTF-8")), "UTF-8"));
+     * }
+     * @param base64body 
+     */
+    public void setBase64body(String base64body)
+    {
+        this.base64body = base64body;
+    }
 
 }
