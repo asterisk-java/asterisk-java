9c9e9d41f46a02938be2124b9cd27af39e4c9a6d
Paulo Nascimento <paulokinho@terra.com.br>
2019-08-22 19:42:59 -0300

    Fixed ManageErrors ClassClastException (#309)

--- src/main/java/org/asteriskjava/pbx/asterisk/wrap/response/CommandResponse.java
+++ src/main/java/org/asteriskjava/pbx/asterisk/wrap/response/CommandResponse.java
@@ -1,21 +1,33 @@
 package org.asteriskjava.pbx.asterisk.wrap.response;
 
+import java.util.Collections;
 import java.util.List;
 
 public class CommandResponse extends ManagerResponse
 {
 
     private List<String> result;
+	private boolean error;
 
-    public CommandResponse(org.asteriskjava.manager.response.CommandResponse response)
+	public CommandResponse(org.asteriskjava.manager.response.ManagerResponse response)
     {
         super(response);
-        this.result = response.getResult();
 
+        if (response instanceof org.asteriskjava.manager.response.CommandResponse) {
+	        result = ((org.asteriskjava.manager.response.CommandResponse) response).getResult();
+        } else if (response instanceof org.asteriskjava.manager.response.ManagerError) {
+        	error = true;
+        	result = Collections.singletonList(response.getOutput());
+        }
     }
 
     public List<String> getResult()
     {
         return this.result;
     }
+
+	public boolean isError()
+	{
+		return error;
+	}
 }
