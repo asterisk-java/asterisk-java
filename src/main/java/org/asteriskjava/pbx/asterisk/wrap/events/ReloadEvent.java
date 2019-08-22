955db85eb68cf8a4cb98a292c1620eec779793c8
Paulo Nascimento <paulokinho@terra.com.br>
2019-08-22 19:51:25 -0300

    Feature/reload event (#311)
    * Fixed ManageErrors ClassClastException
    
    * Added method to check if a dialplan for a context exists
    
    * Added ReloadEvent class to listen for "core reload" events

---
+++ src/main/java/org/asteriskjava/pbx/asterisk/wrap/events/ReloadEvent.java
@@ -0,0 +1,8 @@
+package org.asteriskjava.pbx.asterisk.wrap.events;
+
+public class ReloadEvent extends ManagerEvent {
+
+    public ReloadEvent(final org.asteriskjava.manager.event.ReloadEvent event) {
+        super(event);
+    }
+}
