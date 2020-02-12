f1e46de7accde45dcddf6a647a7a7634d96a9ccf
Robert Sutton <rsutton@noojee.com.au>
2020-02-12 11:22:39 +1100

    work on release script

--- triggerRelease.dart
+++ triggerRelease.dart
@@ -13,6 +13,7 @@ void main() {
   print(dir);
   read(join(dir, "pom.xml")).forEach((line) {
     if (line.contains("<releaseVersion>")) {
+      line = line.replaceFirst("-SNAPSHOT", "");
       var parts = line.split(".");
       if (parts.length != 3) {
         exit(1);
@@ -22,9 +23,17 @@ void main() {
       rev = int.parse(parts[2].split("<")[0]);
     }
   });
-  rev++;
 
-  String version = "$major.$minor.$rev";
+  var postFix = "";
+  if (confirm(prompt: "Is this a SNAPSHOT release")) {
+    postFix = "-SNAPSHOT";
+    rev++;
+  } else {
+    rev = 0;
+    minor++;
+  }
+
+  String version = "$major.$minor.$rev$postFix";
 
   replace(join(dir, "pom.xml"), version);
 
@@ -35,7 +44,7 @@ void main() {
   'git push origin tag $version'.run;
 }
 
-void replace(String path, version) {
+void replace(String path, String version) {
   var tmp = '$path.tmp';
   if (exists(tmp)) {
     delete(tmp);
