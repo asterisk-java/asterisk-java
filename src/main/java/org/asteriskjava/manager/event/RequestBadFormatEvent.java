aa33baf66b14b158068afe61d792b544e0f69b95
patryk <patryk.galwa@linktechnologies.pl>
2020-09-14 11:16:40 +0200

    Added RequestBadFormatEvent, triggered when asterisk was restarted and action was produced in the same time.

---
+++ src/main/java/org/asteriskjava/manager/event/RequestBadFormatEvent.java
@@ -0,0 +1,117 @@
+package org.asteriskjava.manager.event;
+
+/**
+ * A RequestBadFormatEvent is triggered when bad format of request.
+ */
+
+public class RequestBadFormatEvent extends ManagerEvent {
+
+	/**
+	 * Serializable version identifier.
+	 */
+	private final static long serialVersionUID = 12534663L;
+
+	private String severity;
+
+	private Integer eventversion;
+
+	private String sessiontv;
+
+	private String eventtv;
+
+	private String sessionid;
+
+	private String localaddress;
+
+	private String accountid;
+
+	private String requesttype;
+
+	private String service;
+
+	private String remoteaddress;
+
+	public RequestBadFormatEvent(Object source) {
+		super(source);
+	}
+
+	public String getSeverity() {
+		return severity;
+	}
+
+	public void setSeverity(String severity) {
+		this.severity = severity;
+	}
+
+	public Integer getEventversion() {
+		return eventversion;
+	}
+
+	public void setEventversion(Integer eventversion) {
+		this.eventversion = eventversion;
+	}
+
+	public String getSessiontv() {
+		return sessiontv;
+	}
+
+	public void setSessiontv(String sessiontv) {
+		this.sessiontv = sessiontv;
+	}
+
+	public String getEventtv() {
+		return eventtv;
+	}
+
+	public void setEventtv(String eventtv) {
+		this.eventtv = eventtv;
+	}
+
+	public String getSessionid() {
+		return sessionid;
+	}
+
+	public void setSessionid(String sessionid) {
+		this.sessionid = sessionid;
+	}
+
+	public String getLocaladdress() {
+		return localaddress;
+	}
+
+	public void setLocaladdress(String localaddress) {
+		this.localaddress = localaddress;
+	}
+
+	public String getAccountid() {
+		return accountid;
+	}
+
+	public void setAccountid(String accountid) {
+		this.accountid = accountid;
+	}
+
+	public String getRequesttype() {
+		return requesttype;
+	}
+
+	public void setRequesttype(String requesttype) {
+		this.requesttype = requesttype;
+	}
+
+	public String getService() {
+		return service;
+	}
+
+	public void setService(String service) {
+		this.service = service;
+	}
+
+	public String getRemoteaddress() {
+		return remoteaddress;
+	}
+
+	public void setRemoteaddress(String remoteaddress) {
+		this.remoteaddress = remoteaddress;
+	}
+}
