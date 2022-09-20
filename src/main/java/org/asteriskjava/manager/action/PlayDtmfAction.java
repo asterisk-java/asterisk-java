cb283e0377d6d104105caaf037ecf8ea1fae8b92
Robert Sutton <rlsutton152@gmail.com>
2022-09-20 12:48:49 +1000

    allow setting dtmf duration on playDtmfAction

--- src/main/java/org/asteriskjava/manager/action/PlayDtmfAction.java
+++ src/main/java/org/asteriskjava/manager/action/PlayDtmfAction.java
@@ -17,8 +17,10 @@
 package org.asteriskjava.manager.action;
 
 /**
- * The PlayDTMFAction plays a DTMF digit on the specified channel.<p>
- * It is definied in <code>apps/app_senddtmf.c</code>.<p>
+ * The PlayDTMFAction plays a DTMF digit on the specified channel.
+ * <p>
+ * It is definied in <code>apps/app_senddtmf.c</code>.
+ * <p>
  * Available since Asterisk 1.2.8
  * 
  * @since 0.3
@@ -27,80 +29,96 @@ package org.asteriskjava.manager.action;
  */
 public class PlayDtmfAction extends AbstractManagerAction
 {
-    /**
-     * Serializable version identifier
-     */
-    private static final long serialVersionUID = 9002288048692675696L;
+	/**
+	 * Serializable version identifier
+	 */
+	private static final long serialVersionUID = 9002288048692675696L;
 
-    private String channel;
-    private String digit;
+	private String channel;
+	private String digit;
+	private Integer duration = null;
 
-    /**
-     * Creates a new empty PlayDtmfAction.
-     */
-    public PlayDtmfAction()
-    {
+	/**
+	 * Creates a new empty PlayDtmfAction.
+	 */
+	public PlayDtmfAction()
+	{
 
-    }
+	}
 
-    /**
-     * Creates a new PlayDtmfAction that sends the given DTMF digit to the given channel.
-     * 
-     * @param channel the name of the channel to send the digit to.
-     * @param digit the DTML digit to play.
-     */
-    public PlayDtmfAction(String channel, String digit)
-    {
-        this.channel = channel;
-        this.digit = digit;
-    }
+	/**
+	 * Creates a new PlayDtmfAction that sends the given DTMF digit to the given
+	 * channel.
+	 * 
+	 * @param channel
+	 *            the name of the channel to send the digit to.
+	 * @param digit
+	 *            the DTML digit to play.
+	 */
+	public PlayDtmfAction(String channel, String digit)
+	{
+		this.channel = channel;
+		this.digit = digit;
+	}
 
-    /**
-     * Returns the name of this action, i.e. "PlayDTMF".
-     */
-    @Override
-   public String getAction()
-    {
-        return "PlayDTMF";
-    }
+	/**
+	 * Returns the name of this action, i.e. "PlayDTMF".
+	 */
+	@Override
+	public String getAction()
+	{
+		return "PlayDTMF";
+	}
 
-    /**
-     * Returns the name of the channel to send the digit to.
-     * 
-     * @return the name of the channel to send the digit to.
-     */
-    public String getChannel()
-    {
-        return channel;
-    }
+	/**
+	 * Returns the name of the channel to send the digit to.
+	 * 
+	 * @return the name of the channel to send the digit to.
+	 */
+	public String getChannel()
+	{
+		return channel;
+	}
 
-    /**
-     * Sets the name of the channel to send the digit to.
-     * 
-     * @param channel the name of the channel to send the digit to.
-     */
-    public void setChannel(String channel)
-    {
-        this.channel = channel;
-    }
+	/**
+	 * Sets the name of the channel to send the digit to.
+	 * 
+	 * @param channel
+	 *            the name of the channel to send the digit to.
+	 */
+	public void setChannel(String channel)
+	{
+		this.channel = channel;
+	}
 
-    /**
-     * Returns the DTMF digit to play.
-     * 
-     * @return the DTMF digit to play.
-     */
-    public String getDigit()
-    {
-        return digit;
-    }
+	/**
+	 * Returns the DTMF digit to play.
+	 * 
+	 * @return the DTMF digit to play.
+	 */
+	public String getDigit()
+	{
+		return digit;
+	}
 
-    /**
-     * Sets the DTMF digit to play.
-     * 
-     * @param digit the DTMF digit to play.
-     */
-    public void setDigit(String digit)
-    {
-        this.digit = digit;
-    }
+	/**
+	 * Sets the DTMF digit to play.
+	 * 
+	 * @param digit
+	 *            the DTMF digit to play.
+	 */
+	public void setDigit(String digit)
+	{
+		this.digit = digit;
+	}
+
+	public void setDuration(Integer duration)
+	{
+		this.duration = duration;
+	}
+
+	public Integer getDuration()
+	{
+		return duration;
+	}
 }
