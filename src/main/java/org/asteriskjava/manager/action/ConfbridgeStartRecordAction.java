8192a6d8d78757c44b66921794d505d78beec9eb
Piotr Olaszewski <piotroo89@gmail.com>
2022-11-16 07:59:50 +0100

    Add recordFile field to ConfbridgeStartRecordAction

--- src/main/java/org/asteriskjava/manager/action/ConfbridgeStartRecordAction.java
+++ src/main/java/org/asteriskjava/manager/action/ConfbridgeStartRecordAction.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2004-2022 Asterisk-Java contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.asteriskjava.manager.action;
 
 /**
@@ -7,13 +22,13 @@ package org.asteriskjava.manager.action;
  * @version $Id$
  */
 public class ConfbridgeStartRecordAction extends AbstractManagerAction {
-
     /**
      * Serializable version identifier
      */
     private static final long serialVersionUID = 3059632508521358701L;
 
     private String conference;
+    private String recordFile;
 
     /**
      * Creates a new empty ConfbridgeStartRecordAction.
@@ -51,4 +66,11 @@ public class ConfbridgeStartRecordAction extends AbstractManagerAction {
         return conference;
     }
 
+    public String getRecordFile() {
+        return recordFile;
+    }
+
+    public void setRecordFile(String recordFile) {
+        this.recordFile = recordFile;
+    }
 }
