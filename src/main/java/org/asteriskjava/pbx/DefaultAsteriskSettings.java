3ae3318f3fc39d7dcdc8bd2a0d0a7d3e9d55bfa4
shubham-910 <shubhamjethva92@gmail.com>
2023-11-23 17:10:14 -0400

    changes in the files

--- src/main/java/org/asteriskjava/pbx/DefaultAsteriskSettings.java
+++ src/main/java/org/asteriskjava/pbx/DefaultAsteriskSettings.java
@@ -59,7 +59,8 @@ public abstract class DefaultAsteriskSettings implements AsteriskSettings {
     public int getDialTimeout() {
         // Return the amount of time to wait (in seconds) when dialing before we give up
         // when the call isn't answered
-        return 30;
+        int defaultTimeout = 30;
+        return defaultTimeout;
     }
 
     /*
