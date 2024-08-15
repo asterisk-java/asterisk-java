82bd6e20856772ef094afba36494001349a95442
Sean Bright <sean@seanbright.com>
2024-08-15 17:17:57 -0400

    AsteriskVersion.java: Add Asterisk v22 and missing AMI versions.

--- src/main/java/org/asteriskjava/AsteriskVersion.java
+++ src/main/java/org/asteriskjava/AsteriskVersion.java
@@ -27,6 +27,7 @@ import java.util.regex.Pattern;
  * @since 0.2
  */
 public class AsteriskVersion implements Comparable<AsteriskVersion>, Serializable {
+    private static final String VERSION_PATTERN_22 = "^\\s*Asterisk (GIT-)?22[-. ].*";
     private static final String VERSION_PATTERN_21 = "^\\s*Asterisk (GIT-)?21[-. ].*";
     private static final String VERSION_PATTERN_20 = "^\\s*Asterisk (GIT-)?20[-. ].*";
     private static final String VERSION_PATTERN_19 = "^\\s*Asterisk (GIT-)?19[-. ].*";
@@ -162,9 +163,16 @@ public class AsteriskVersion implements Comparable<AsteriskVersion>, Serializabl
      */
     public static final AsteriskVersion ASTERISK_21 = new AsteriskVersion(2100, "Asterisk 21", VERSION_PATTERN_21);
 
+    /**
+     * Represents the Asterisk 22 series.
+     *
+     * @since 3.40.0
+     */
+    public static final AsteriskVersion ASTERISK_22 = new AsteriskVersion(2200, "Asterisk 22", VERSION_PATTERN_22);
+
     private static final AsteriskVersion knownVersions[] = new AsteriskVersion[]{
-        ASTERISK_21, ASTERISK_20, ASTERISK_19, ASTERISK_18, ASTERISK_17, ASTERISK_16, ASTERISK_15, ASTERISK_14,
-        ASTERISK_13, ASTERISK_12, ASTERISK_11, ASTERISK_10, ASTERISK_1_8, ASTERISK_1_6
+        ASTERISK_22, ASTERISK_21, ASTERISK_20, ASTERISK_19, ASTERISK_18, ASTERISK_17, ASTERISK_16, ASTERISK_15,
+        ASTERISK_14, ASTERISK_13, ASTERISK_12, ASTERISK_11, ASTERISK_10, ASTERISK_1_8, ASTERISK_1_6
     };
 
     // current debian stable version, as of 09/10/2018
